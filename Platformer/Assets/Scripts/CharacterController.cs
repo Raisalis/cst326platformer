6c603ae00efa94ac220356b3977fa95b7d71eced
Emma <71865150+Raisalis@users.noreply.github.com>
2022-02-28 16:50:10 -0800

    Animations added
    Animator + Animations added to character. Script for movement edited.

--- Platformer/Assets/Scripts/CharacterController.cs
+++ Platformer/Assets/Scripts/CharacterController.cs
@@ -5,67 +5,81 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float runForce = 50f;
-    public float maxRunSpeed = 6f;
-    public float jumpForce = 20f;
-    private Rigidbody body;
-    public bool feetOnGround;
-    private Collider collider;
-    public float maxJumpForce = 40f;
-    public bool jumpCheck;
-    private int jumpCount;
+    public float jumpImpulseForce = 20f;
+    public float jumpSustainForce = 7.5f;
+    public bool feetOnGround = false;
+    public float maxHorizontalSpeed = 6f;
+    private Animator animComp;
+    private int jumpCounter = 0;
+    public bool jumping = false;
+    public bool forward = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
-        jumpCheck = false;
-        jumpCount = 0;
+        
+        animComp = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Jump
-        if(jumpCheck)
-        {
-            if(Input.GetKey(KeyCode.Space) && jumpCount < 20)
-            {
-                body.AddForce(Vector3.up * maxJumpForce, ForceMode.Force);
-                jumpCount++;
-            }
-            else
-            {
-                jumpCount = 0;
-                jumpCheck = false;
-            }
-        }
+
         // Checks if character is on the ground
+        Collider collider = GetComponent<Collider>();
         float castDistance = collider.bounds.extents.y + 0.1f;
         feetOnGround = Physics.Raycast(transform.position, Vector3.down, castDistance);
 
+        if(feetOnGround && jumpCounter != 0)
+        {
+            jumpCounter = 0;
+            jumping = false;
+        }
+
         //Add force based on user input along horizontal axis
         float axis = Input.GetAxis("Horizontal");
+        Rigidbody body = GetComponent<Rigidbody>();
         body.AddForce(Vector3.right * axis * runForce, ForceMode.Force);
-
-        //Cap the run speed only if you're running, not if you're falling.
-        if(Mathf.Abs(body.velocity.x) > maxRunSpeed)
+        if(axis > 0  && forward == false)
         {
-            float newX = maxRunSpeed * Mathf.Sign(body.velocity.x);
-            body.velocity = new Vector3(newX, body.velocity.y, body.velocity.z);
+            transform.Rotate(0f, 180f, 0f);
+            forward = true;
+        }
+        else if(axis < 0 && forward == true)
+        {
+            transform.Rotate(0f, -180f, 0f);
+            forward = false;
         }
 
         //Jump
         if(Input.GetKeyDown(KeyCode.Space) && feetOnGround)
         {
-            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpCheck = true;
+            body.AddForce(Vector3.up * jumpImpulseForce, ForceMode.Impulse);
+            jumpCounter++;
+            jumping = true;
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            if(jumpCounter < 100)
+            {
+                body.AddForce(Vector3.up * jumpSustainForce, ForceMode.Force);
+                jumpCounter++;
+            }
         }
 
+        // Check the velocity along x axis and clamp it if character is moving faster than max speed.
+        float xVelocity = Mathf.Clamp(body.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
+
+        // If player isn't holding down a run button, start slowing down the character.
         if(Mathf.Abs(axis) < 0.1f)
         {
-            float newX = body.velocity.x * (1f - Time.deltaTime * 3f);
-            body.velocity = new Vector3(newX, body.velocity.y, body.velocity.z);
+            xVelocity *= 0.9f;
         }
+
+        body.velocity = new Vector3(xVelocity, body.velocity.y, body.velocity.z);
+
+        animComp.SetFloat("Speed", body.velocity.magnitude);
+        animComp.SetBool("Jumping", jumping);
     }
 }
